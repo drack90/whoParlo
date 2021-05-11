a318d0801090409ba6e18a0e1fc2fb97a90ee534
Sergey Zavodov <drack702@gmail.com>
2021-05-11 19:47:33 +0500

    -1

--- src/components/workspace/getDataLessonComponents/answerList.vue
+++ src/components/workspace/getDataLessonComponents/answerList.vue
@@ -1,14 +1,39 @@
 <template>
 <div>
-				<div class="py-3">
+				<div class="py-3" v-if="edit">
 					<div class="info-block info-block__big" 
-						name="lessonData" 
+						name="lessonData"
 						id="lessonData">
 						<p v-for="(dateLesson, index) in getAllAnswer[pickFlow]" :key="index">
 							<b>{{index}}:</b> <br>
 							<span v-for="name in dateLesson" :key="name">{{name}}, </span></p>
-					</div>
+          </div>
+          <b-button   class="mt-3 "
+                      variant="outline-primary"
+                      @click="edit = !edit">
+                      Редактировать
+                      </b-button>
       </div>
+      <div class="py-3" v-else>
+              <div>
+                 <b-input
+                  name="lessonData"
+                  id="lessonData">
+                  Тут будет этот текст `${edit}`
+                      <!-- <p v-for="(dateLesson, index) in getAllAnswer[pickFlow]" :key="index">
+                      <b>{{index}}:</b> <br>
+                      <span v-for="name in dateLesson" :key="name">{{name}}, </span></p> -->
+				        	</b-input>
+              </div>
+              <b-button   class="mt-3 justify-content-end"
+                      variant="success"
+                      @click="addDataToBase">
+                      Сохранить
+                      </b-button>
+            </div>
+
+            <div>{{getAllAnswer[pickFlow]}}</div>
+            <b-button variant="success" @click="logThisAnswer">Log</b-button>
 </div>
     
 </template>
@@ -19,17 +44,26 @@ export default{
     name: 'AnswerList',
     data(){ 
         return{
-            
+            edit: true,
+            test: '',
         }
     },
 
     methods:{
-
+        addDataToBase(){
+            this.edit = !this.edit
+        },
+        logThisAnswer(){
+          let test = this.$store.getters.getAllAnswer[this.$store.getters.getPickFlow]
+          test = JSON.stringify(test)
+          this.test = test
+          console.log(this.getAllAnswer[this.pickFlow])
+        }
     },
     
     
     mounted(){
-  
+       
 
     },
     watch: {
@@ -39,6 +73,7 @@ export default{
 
     computed:{
 			getAllAnswer(){
+        
       	return this.$store.getters.getAllAnswer
     	},
 			pickFlow(){
