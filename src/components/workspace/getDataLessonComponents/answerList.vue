ba170598a5149552bc0945d74a159707071a1f64
Sergey Zavodov <drack702@gmail.com>
2021-05-13 22:07:19 +0500

    -создал редактирование
    -Создал редактирование данных в потоке через jsoneditor

--- src/components/workspace/getDataLessonComponents/answerList.vue
+++ src/components/workspace/getDataLessonComponents/answerList.vue
@@ -10,71 +10,108 @@
           </div>
           <b-button   class="mt-3 "
                       variant="outline-primary"
-                      @click="edit = !edit">
+                      @click="editJSONMethods">
                       Редактировать
                       </b-button>
       </div>
-      <div class="py-3" v-else>
-              <div>
-                 <b-input
-                  name="lessonData"
-                  id="lessonData">
-                  Тут будет этот текст `${edit}`
-                      <!-- <p v-for="(dateLesson, index) in getAllAnswer[pickFlow]" :key="index">
-                      <b>{{index}}:</b> <br>
-                      <span v-for="name in dateLesson" :key="name">{{name}}, </span></p> -->
-				        	</b-input>
-              </div>
+      <div class="py-3" v-show =" edit == false">
+              <div id="jsonElditor" style="width: 100%; height: 400px;"></div>
               <b-button   class="mt-3 justify-content-end"
                       variant="success"
-                      @click="addDataToBase">
+                      @click="addDataToBase"
+                      >
                       Сохранить
                       </b-button>
             </div>
 
-            <div>{{getAllAnswer[pickFlow]}}</div>
-            <b-button variant="success" @click="logThisAnswer">Log</b-button>
 </div>
     
 </template>
 
 <script>
 
+import JSONEditor from 'jsoneditor'
 export default{
     name: 'AnswerList',
     data(){ 
         return{
             edit: true,
             test: '',
+            toast: null,
+            //переменные для JSONEditor
+            container:'',
+            options: '', 
+            editor: '',
+            editJSON: '',
         }
     },
-
-    methods:{
-        addDataToBase(){
-            this.edit = !this.edit
-        },
-        logThisAnswer(){
-          let test = this.$store.getters.getAllAnswer[this.$store.getters.getPickFlow]
-          test = JSON.stringify(test)
-          this.test = test
-          console.log(this.getAllAnswer[this.pickFlow])
-        }
-    },
-    
-    
     mounted(){
-       
+      //создаем переменные для JSONEditor
+          this.container = document.getElementById('jsonElditor')
+          this.options = {
+            mode: 'tree'
+          }
+          this.editor = new JSONEditor(this.container, this.options)
+    },
+    methods:{
+      //функция меняет значение кнопки скрывая эдитор
+      //после чего производит запись измененных данных в БД
+        async addDataToBase(){
+            let pickFlows = await this.pickFlow
+            this.editJSON = await this.editor.get()
+            await this.$store.dispatch('updateAnswerStudentsInFlow', {
+              data: this.editJSON,
+              pickFlows: pickFlows
+              })
+            this.edit = !this.edit
+             await this.$store.dispatch('fetchAllAnswer');
+        },
+        //Производим инициализацию JSONEditor и загружаем в него данные из выбранного потока
+       async editJSONMethods(){
+          let pickFlows = await this.pickFlow
+          if (await pickFlows == '') {
+            this.viewToast('Ну выбран поток для редактирования')
+          }else{
+            this.editor.set(this.getAllAnswer[this.pickFlow])
+            this.edit = !this.edit
+          }
+
+          
+          
+        },
+        //метод позволяет вызвать toast с ошибкой.
+        viewToast(message){
+            this.$bvToast.toast(`${message}`,{
+            title: 'Ошибка',
+            message: message,
+            variant: 'danger',
+            autoHideDelay: 1000,
+            solid: true,
+            toaster: "b-toaster-bottom-right",
+          })
+        }
 
     },
-    watch: {
-     
+    watch:{
+      //следим за изменениями в переменной vuex и при изменении выводим сообщение
+        getAnswerToast(){
+            this.$bvToast.toast(`${this.getAnswerToast.message}`,{
+            title: this.getAnswerToast.title,
+            variant: this.getAnswerToast.variant,
+            autoHideDelay: 1000,
+            solid: true,
+            toaster: "b-toaster-bottom-right",
+          })
+        },
     },
-    
+
 
     computed:{
+      getAnswerToast(){
+      return this.$store.getters.getAnswerToast
+      },
 			getAllAnswer(){
-        
-      	return this.$store.getters.getAllAnswer
+      return this.$store.getters.getAllAnswer
     	},
 			pickFlow(){
       return this.$store.getters.getPickFlow
--- src/store/modules/dataLesson.js
+++ src/store/modules/dataLesson.js
@@ -2,76 +2,114 @@ import { database } from "../../require/firebase";
 
 export default {
     state: {
-      allAnswer: [],
-      answerData: [],
-      answerStudent:[],
-      answerStudents:[],
-      asnwerStudentArr:[],
+        allAnswer: [],
+        answerData: [],
+        answerStudent: [],
+        answerStudents: [],
+        answerStudentArr: [], //Данные из массива "кто отвечал"
+        updateAnswerStudent: [],
+        toast: null,
 
     },
     getters: {
-      getAllAnswer(state){
-        return state.allAnswer
-      },
-      getAnswerData(state){
-         return state.answerData
-      },
-      getAnswerStudent(state){
-        return state.answerStudent
-      },
-      getAnswerStudents(state){
-        return state.answerStudents
-      },
-      getAnswerStudents(state){
-        return state.answerStudentsArr
-      }
+        getAnswerToast(state) {
+            return state.toast
+        },
+        getAllAnswer(state) {
+            return state.allAnswer
+        },
+        getAnswerData(state) {
+            return state.answerData
+        },
+        getAnswerStudent(state) {
+            return state.answerStudent
+        },
+        getAnswerStudents(state) {
+            return state.answerStudents
+        },
+        getAnswerStudentsArr(state) {
+            return state.answerStudentArr
+        },
+        getUpdateAnswerStudent(state) {
+            return state.updateAnswerStudent
+        }
     },
 
     actions: {
-       async fetchAllAnswer(ctx){
-        let allAnswer;
-        let ref = database.ref('answer')
-        await ref.once('value', (snapshot) =>{
-          allAnswer = snapshot.val()
-        })
-        await ctx.commit('updateAllAnswer', allAnswer)
-      },
+        async fetchAllAnswer(ctx) {
+            let allAnswer;
+            let ref = database.ref('answer')
+            await ref.once('value', (snapshot) => {
+                allAnswer = snapshot.val()
+            })
+            await ctx.commit('updateAllAnswer', allAnswer)
+        },
 
-      async fetchAllAnswerArr(ctx, pickFlow){
-        let allAnswerArr;
-        let result;
-        let ref = database.ref('answer/' + pickFlow)
-        await ref.once('value', (snapshot) =>{
-          allAnswerArr = snapshot.val()
-        })
-        for (const group in allAnswerArr) { 
-          for(const index in allAnswerArr[group]){
-           // Vue.set(this.search, this.compileData[group][index])
-            this.search.push(allAnswerArr[group][index])
-          }                   
+        async fetchAllAnswerArr(ctx, pickFlow) {
+            let allAnswerArr;
+            let result;
+            let ref = database.ref('answer/' + pickFlow)
+            await ref.once('value', (snapshot) => {
+                allAnswerArr = snapshot.val()
+            })
+            for (const group in allAnswerArr) {
+                for (const index in allAnswerArr[group]) {
+                    // Vue.set(this.search, this.compileData[group][index])
+                    this.search.push(allAnswerArr[group][index])
+                }
+            }
+            await ctx.commit('updateAllAnswerArr', result)
+        },
+        //метод записи данных из редактора JSON в базу
+        async updateAnswerStudentsInFlow(ctx, newAnswerStudentArr) {
+
+            let ref = database.ref('answer/' + newAnswerStudentArr.pickFlows)
+            ref.set(newAnswerStudentArr.data, (error) => {
+                if (error) {
+                    return alert(error);
+                } else {
+                    let toastText = {
+                        message: 'Данные обновлены',
+                        title: 'Успешно!',
+                        variant: 'success'
+                    }
+                    ctx.commit('updateAnswerToast', toastText)
+                }
+            });
+
+
+        },
+
+        updateToast(ctx) {
+            ctx.commit('updateAnswerToast', null)
+        },
+
+        updateAnswerStudentsArrAction(ctx, answeredArr) {
+            ctx.commit('updateAnswerStudentsArr', answeredArr)
         }
-        await ctx.commit('updateAllAnswerArr', result)
-      },
 
     },
     mutations: {
-      updateAllAnswer(state, allAnswer){
-        state.allAnswer = allAnswer
-      },
-      updateAnswerData(){
+        updateAnswerToast(state, toast) {
+            state.toast = toast
+        },
+        updateAllAnswer(state, allAnswer) {
+            state.allAnswer = allAnswer
+        },
+        updateAnswerData() {
 
-      },
-      updateAnswerStudent(){
+        },
+        updateAnswerStudent() {
 
-      },
-      updateAnswerStudents(){
+        },
+        updateAnswerStudents() {
 
-      },
-      updateAnswerStudentsArr(state, allAnswerArr){
-        state.allAnswerArr = allAnswerArr
-      },
+        },
+        updateAnswerStudentsArr(state, allAnswerArr) {
+            state.answerStudentArr = allAnswerArr
+        },
 
     },
-    
-   
+
+
 }
